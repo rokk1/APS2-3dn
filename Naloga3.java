78675626e9dcdf59075cec68b412a04b22fa0d28
rokk1 <rk0881@student.uni-lj.si>
2019-05-22 13:33:47 +0200

    Implementacija switch stavka

--- Naloga3.java
+++ Naloga3.java
@@ -1,9 +1,28 @@
 import java.util.*;
 public class Naloga3 {
 	
-
+	public static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
 
-		
+		String alg = in.next();
+		System.out.printf("%s\n", alg);
+
+		switch (alg) {
+			case ("2c"):
+				System.out.println("2c");
+				break;
+			case ("gr"):
+				System.out.println("gr");
+				break;
+			case ("ex"):
+				System.out.println("ex");
+				break;
+			case ("bt"):
+				System.out.println("bt");
+				break;
+			default:
+			System.out.println("dp");
+				break;
+		}
 	}
 }
